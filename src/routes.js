739a7c25b3858b80f1f46c9148f6efdf3b4fe710
Ricardo Brasil <ricardo@grupocm.net>
2019-09-30 18:06:01 -0300

    adicionado o model User, UserController e a migration User

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Olá Brasil' }));
+routes.post('/users', UserController.store);
 export default routes;
