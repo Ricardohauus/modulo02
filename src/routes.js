57e8bf02d54e9de7e96ea69248de22d686dc19fc
Ricardo Brasil <ricardohauus@gmail.com>
2019-10-29 18:07:05 -0300

    adicionando horário disponiveis para agendamento

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ProviderController from './app/controllers/ProviderController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -21,6 +22,7 @@ routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/providers', ProviderController.index);
 routes.get('/notifications', NotificationController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 routes.put('/notifications/:id', NotificationController.update);
 routes.delete('/appointments/:id', AppointmentController.delete);
 export default routes;
